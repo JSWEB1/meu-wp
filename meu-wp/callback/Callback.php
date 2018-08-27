9f8990284a2edc52a1640e44331aef001cd3abfc
Pedro Mázala <pedroh.mazala@gmail.com>
2018-08-27 13:00:10 -0300

    Save data on logs on the right way

--- meu-wp/callback/Callback.php
+++ meu-wp/callback/Callback.php
@@ -1,24 +1,22 @@
-<?php  
+<?php
 // URL de callback: {{SEUSITE}}/wp-content/plugins/meu-wp/callback/Callback.php
-  if (isset($_POST)) {
-    $myfile = fopen("logDoPost.txt", "a") or die("não deu!");
-    $txt = "".$_POST.PHP_EOL;
-    $printr = var_export($_POST, true);
-   
-    fwrite($myfile, $txt . $printr);
+
+if (isset($_POST)) {
+    $myfile = fopen("logDoPost.txt", "a+") or die("não deu!");
+    $txt = var_export($_POST, true) . PHP_EOL;
+
+    fwrite($myfile, $txt);
     fclose($myfile);
     if (isset($_POST['ORDER'])) {
-      $myfile = fopen("logDoGetOrder.txt", "w") or die("não deu!");
-      $txt = "".var_dump($_POST['ORDER']);
-      fwrite($myfile, $txt);
-      fclose($myfile);
+        $myfile = fopen("logDoGetOrder.txt", "w+") or die("não deu!");
+        $txt = "" . var_dump($_POST['ORDER']);
+        fwrite($myfile, $txt);
+        fclose($myfile);
     }
-  }  
-  if (isset($_GET)) {
-    $myfile = fopen("logDoGet.txt", "a") or die("não deu!");
-    $txt = "".$_GET.PHP_EOL;
-    $printr = var_export($_GET, true);
-    fwrite($myfile, $txt . $printr);
+}
+if (isset($_GET)) {
+    $myfile = fopen("logDoGet.txt", "a+") or die("não deu!");
+    $txt = var_export($_GET, true) . PHP_EOL;
+    fwrite($myfile, $txt);
     fclose($myfile);
-  }
-?>
+}
