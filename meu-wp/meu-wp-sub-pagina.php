38978c3881a12b9217a1900e88956cf452f7fb93
JSWEB1 <deanstudio@hotmail.com>
2018-08-28 02:43:59 -0300

    v1

--- meu-wp/meu-wp-sub-pagina.php
+++ meu-wp/meu-wp-sub-pagina.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
     <!-- Popper JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
     <!-- Latest compiled JavaScript -->
@@ -27,6 +28,44 @@
     	function myFunction(message) 
     	{
 		    alert(message);
+		}
+
+		function postProds(){
+			var retorno = '';
+			var result = false;
+			$.ajax({
+				type:"post",
+				url: "",
+				data:"expprod=true",
+				cache:false,
+				success: function(){
+					result = true;
+					alert('Enviou');
+				},
+				error: function (retorno) {
+    				alert('Erro ao Enviar' + retorno);
+  				}
+			});
+			return false;
+		}		
+
+		function postCats(){
+			var retorno = '';
+			var result = false;
+			$.ajax({
+				type:"post",
+				url: "",
+				data:"expcat=true",
+				cache:false,
+				success: function(){
+					result = true;
+					alert('Enviou');
+				},
+				error: function (retorno) {
+    				alert('Erro ao Enviar' + retorno);
+  				}
+			});
+			return false;
 		}
     </script>
 </head>
@@ -109,11 +148,12 @@
 						</div>
 			 			<!--Exportar todas as categorias -->
 		  	 			<!--<form action="<?php /*echo plugin_url().'/meu-wp/integracao/'*/?>" method="post">-->
-		  	 			<form action="" method="post">
+		  	 			<!-- <form action="" method="post"> -->
+		  	 			<form>
 	         				<dt style="text-align:center">
 							<label style="">Limite por envio (20 a 100)<input type="text"  class="form-control" name="catwooli" value="<?php echo get_option('limit_catwoo');?>"/></label>
 							<br><br>
-							<button style="width:230px" type="submit"	class="btn btn-primary" id="expcat" name="expcat" >
+							<button onclick="return postCats()" style="width:230px" type="submit"	class="btn btn-primary" id="expcat" name="expcat" >
 							  <span class="glyphicon glyphicon-export"></span>
 							  <b>Exportar todas</b>
 							</button>
@@ -125,14 +165,15 @@
 		 				<div class="alert alert-info"  style="text-align:center">
 	  						Exportação de categoria especifica
 						</div>
-						<form action="" method="post">
+						<!-- <form action="" method="post"> -->
+						<form>
 							<dt style="text-align:center">
 							<label style=""> ID Especifico<input type="text"  class="form-control" name="catwooid" value="<?php echo get_option('id_catwoo');?>"/></label><br>		
 							</dd>
 							</dt>
 							<br>
 							<dt style="text-align:center">
-							<button style="width:230px" type="submit"	class="btn btn-primary"name="Submit" >
+							<button onclick="return postOneCat()" style="width:230px" type="submit"	class="btn btn-primary"name="Submit" >
 							  <span class="glyphicon glyphicon-export"></span>
 							  <b>Exportar</b>
 							</button>
@@ -147,10 +188,11 @@
   			Exportação de produtos
 		</div>
 	  <!--Exportar todos os produtos -->
-      <form action="" method="post">
+      	<!-- <form action="" method="post"> -->
+      	<form>
 	         <dt style="text-align:center">
 			 <label style="">Limite por envio(20 a 100)<input type="text"  class="form-control" name="prodwooli" value="<?php echo get_option('limit_prodwoo');?>"/></label><br><br>
-			<button style="width:230px" type="submit"	class="btn btn-primary" id="expprod" name="expprod" >
+			<button onclick="return postProds()" style="width:230px" type="submit"	class="btn btn-primary" id="expprod" name="expprod" >
 		    <span class="glyphicon glyphicon-export"></span>
 			 <b>Exportar todos</b>
 			 </button>
@@ -159,7 +201,8 @@
 		</form>
 		<br>
 		<!--Exportar produtos especificos -->
-		<form action="" method="post">
+		<!-- <form action="" method="post"> -->
+		<form>
 	 	<div class="alert alert-info"  style="text-align:center">
   			Exportação de produto especifico
 		</div>
@@ -222,13 +265,6 @@
 </form>
 
 </div>
-
-<script>
-$('#expevt').on('click', function(){
-   alert('Aqui você vai fazer seu ajax enviar' )
-   //$.ajax()
-})
-</script>
     </div>
   </div>
 </div>
